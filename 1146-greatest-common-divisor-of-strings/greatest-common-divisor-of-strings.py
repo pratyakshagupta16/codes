d352cc03d1f75bb141700c55cfaedf4088aed1a9
pratyakshagupta16 <pratyakshagupta06@gmail.com>
2026-06-09 23:27:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (94.45%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -0,0 +1,11 @@
+class Solution:
+    def gcdOfStrings(self, str1, str2):
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        a, b = len(str1), len(str2)
+
+        while b:
+            a, b = b, a % b
+
+        return str1[:a]
