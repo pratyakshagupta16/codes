6f808f3892151d7672fe6d38230441dd5c8f0fbb
pratyakshagupta16 <pratyakshagupta06@gmail.com>
2026-06-30 00:01:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, arr, target):
+
+        seen = set()
+
+        for num in arr:
+            if target - num in seen:
+                return True
+            seen.add(num)
+
+        return False
